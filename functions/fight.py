65284fd1913e617988ec29624ec90231f3e8bb1c
Toaster <38060892+TToasterr@users.noreply.github.com>
2018-10-29 08:38:16 -0600

    Commented stuff

--- functions/fight.py
+++ functions/fight.py
@@ -5,6 +5,7 @@ from useitem import useItem
 import sys
 
 def fight(player, monster, lvlmax):
+    #Print the player stats and monster stats, then ask what they'd like to do
     player.printStats()
     newline()
     monster.printStats()
@@ -12,6 +13,10 @@ def fight(player, monster, lvlmax):
     inp = input("What would you like to do? \n")
     bigboi()
 
+    #If they choose attack, choose a random number between their min damage (0) and their max (their main power) <-- ADD BUFFS INTO ACCOUNT
+    #and subtract it from the monsters health. If the monster is dead (at or below 0 health) then end the function and
+    #say they won. If its not, get random damage between 0 and the monsters maximum (depending on the level of the player)
+    #and subtract it from the players health. If the player is dead, end the program. If they arent dead, restart the function.
     if inp == "attack":
         mpdmg = lvlmax[player.level]["playerdamage"] + player.equipped["main"].power
         pdmg = ri(0,mpdmg)
@@ -35,6 +40,8 @@ def fight(player, monster, lvlmax):
             newline()
             sys.exit()
 
+    #If they choose to use an item, then use the item (I just stole the code from the use item script xd)
+    #Issue - It prints their equipped too
     elif inp == "use item":
         useItem(player, lvlmax)
         mdmg = 1 + ri(0,lvlmax[player.level]["monsterdamage"])
