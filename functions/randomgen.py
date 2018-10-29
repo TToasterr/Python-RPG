352907ae518b0faeef4e25a4e202db90e53c0ea8
JONKKKK <40896957+JONKKKK@users.noreply.github.com>
2018-10-29 13:58:09 -0600

    Added comments so i feel like a real coder

--- functions/randomgen.py
+++ functions/randomgen.py
@@ -1,3 +1,4 @@
+#Quality art
 # ─██ ██ ██ ██ ██ ██ ██ ─ ██ ██ ██ ── ── ── ── ── ██ ██ ██ ─ ██ ██ ██ ██ ██ ██ ██ ─
 # ─██ ░░ ░░ ░░ ░░ ░░ ██ ─ ██ ░░ ██ ── ── ── ── ── ██ ░░ ██ ─ ██ ░░ ░░ ░░ ░░ ░░ ██ ─
 # ─██ ░░ ██ ██ ██ ░░ ██ ─ ██ ░░ ██ ── ── ── ── ── ██ ░░ ██ ─ ██ ░░ ██ ██ ██ ░░ ██ ─
@@ -10,23 +11,31 @@
 # ─██ ░░ ░░ ░░ ░░ ░░ ██ ─ ██ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ██ ─ ██ ░░ ░░ ░░ ░░ ░░ ██ ─
 # ─██ ██ ██ ██ ██ ██ ██ ─ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ─ ██ ██ ██ ██ ██ ██ ██ ─
 # ─── ── ── ── ── ── ── ─ ── ── ── ── ── ── ── ── ── ── ── ─ ── ── ── ── ── ── ── ─
+
+#Okay now for the code
 from random import randint as ri
 
 def makeSeedMap():
     global seedmap
+    #Creates the blank map and sets the cursor to it
     seedmap = [[0 for y in range(5)] for x in range(5)]
     x = 2
     y = 2
 
+    #Fills the map with random numbers along a path
+    #These numbers will be turned into chunks later
     for i in range(25):
         try:
+            #If cursor hits the edge, go to center (i have to do this cuz python is the large homo)
             if x >= 26 or y >= 26:
                 x = 2
                 y = 2
             seedmap[y][x] = i+1
         except:
+            #If cursor hits edge, go to center
             x = 2
             y = 2
+        #Cursor goes in a direction
         num = ri(1,4)
         if num == 1:
             x += 1
@@ -37,8 +46,10 @@ def makeSeedMap():
         elif num == 4:
             y -= 1
         else:
-            print(num)
+            #This should never happen
+            print("ya goofed the seedmap congrats")
 
+#Self explanitory
 def printSeedMap():
     for y in range(len(seedmap)):
         for x in range(len(seedmap)):
